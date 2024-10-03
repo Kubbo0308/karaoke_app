501e019f9c0b6dbeadb5f69dedaefdbb83a84ef0
Senritsu420 <kubo3900@gmail.com>
2024-10-03 23:24:11 +0900

    ページ作成

---
+++ src/app/bingo/cooperation/page.tsx
@@ -0,0 +1,5 @@
+import { Text } from "@chakra-ui/react";
+
+export default function Cooperation() {
+  return <Text>協力</Text>;
+}
---
+++ src/app/bingo/match/page.tsx
@@ -0,0 +1,5 @@
+import { Text } from "@chakra-ui/react";
+
+export default function Match() {
+  return <Text>対戦</Text>;
+}
