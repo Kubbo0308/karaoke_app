36b66f72f70ecf7295c13f690ec39dc303c72e2d
Senritsu420 <kubo3900@gmail.com>
2024-10-03 23:36:52 +0900

    メドレー部分作成

--- src/app/medley/page.tsx
+++ src/app/medley/page.tsx
@@ -1,5 +1,31 @@
-import { Text } from "@chakra-ui/react";
+"use client";
+
+import { Button, Flex, Text } from "@chakra-ui/react";
+import React, { useState } from "react";
 
 export default function Medley() {
-  return <Text>メドレー</Text>;
+  const [age, setAge] = useState<number>();
+  const [sex, setSex] = useState<string>();
+
+  const getRandomYear = () => {
+    setAge(Math.floor(Math.random() * (2023 - 1980 + 1)) + 1980);
+  };
+
+  const getRandomGender = () => {
+    const genders = ["male", "female"];
+    setSex(genders[Math.floor(Math.random() * genders.length)]);
+  };
+  return (
+    <>
+      <Text>メドレー</Text>
+      <Flex gap="16px">
+        <Text>{age}</Text>
+        <Text>{sex}</Text>
+      </Flex>
+      <Flex gap="16px">
+        <Button onClick={getRandomYear}>年代</Button>
+        <Button onClick={getRandomGender}>性別</Button>
+      </Flex>
+    </>
+  );
 }
