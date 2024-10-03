b000306b811acd0d087cd0a70e0fe1d8576d0f31
Senritsu420 <kubo3900@gmail.com>
2024-10-04 00:10:23 +0900

    ビンゴの対戦画面の名前追加処理作成

--- src/app/bingo/match/page.tsx
+++ src/app/bingo/match/page.tsx
@@ -1,5 +1,31 @@
-import { Text } from "@chakra-ui/react";
+"use client";
+
+import { Button, Flex, Heading, Input, Text } from "@chakra-ui/react";
+import React, { useState } from "react";
 
 export default function Match() {
-  return <Text>対戦</Text>;
+  const [name, setName] = useState<string>("");
+  const [member, setMember] = useState<string[]>([]);
+  const addMember = (name: string) => {
+    setMember([...member, name]);
+    setName("");
+  };
+  return (
+    <>
+      <Heading>対戦</Heading>
+      <Flex direction="column" gap="16px">
+        {member.map((item: string) => (
+          <Text>{item}</Text>
+        ))}
+      </Flex>
+      <Flex gap="16px">
+        <Input
+          placeholder="名前を入力"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        />
+        <Button onClick={() => addMember(name)}>追加</Button>
+      </Flex>
+    </>
+  );
 }
