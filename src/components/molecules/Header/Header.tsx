4c2c143a5214f31a71cd511d7c4e724f412c25c6
Senritsu420 <kubo3900@gmail.com>
2024-10-28 01:24:25 +0900

    ヘッダーの最大幅をタブレットサイズに調整

--- src/components/molecules/Header/Header.tsx
+++ src/components/molecules/Header/Header.tsx
@@ -1,4 +1,5 @@
 import {
+  Box,
   Flex,
   IconButton,
   Link,
@@ -14,61 +15,62 @@ import { PAGE_INFO } from "@/const";
 
 export const Header = () => {
   return (
-    <Flex
-      justifyContent="space-between"
-      alignItems="center"
-      boxShadow="md"
-      h="50px"
-      px={5}
-      mb={6}
-    >
-      <Link
-        as={NextLink}
-        href={PAGE_INFO.HOME.path}
-        _hover={{ textDecoration: "none", opacity: 0.5 }}
+    <Box boxShadow="md" h="50px" px={5} mb={6}>
+      <Flex
+        justifyContent="space-between"
+        alignItems="center"
+        h="100%"
+        maxW="768px"
+        mx="auto"
       >
-        <Text fontWeight={800} fontSize="18px">
-          カラオケゲーム
-        </Text>
-      </Link>
-      <Menu>
-        <MenuButton
-          as={IconButton}
-          aria-label="Options"
-          icon={<GiHamburgerMenu />}
-          variant="outline"
-        />
-        <MenuList>
-          <Link
-            as={NextLink}
-            href={PAGE_INFO.COOPERATION.path}
-            _hover={{ textDecoration: "none", opacity: 0.5 }}
-          >
-            <MenuItem>協力ビンゴ</MenuItem>
-          </Link>
-          <Link
-            as={NextLink}
-            href={PAGE_INFO.MATCH.path}
-            _hover={{ textDecoration: "none", opacity: 0.5 }}
-          >
-            <MenuItem>対戦ビンゴ</MenuItem>
-          </Link>
-          <Link
-            as={NextLink}
-            href={PAGE_INFO.MEDLEY.path}
-            _hover={{ textDecoration: "none", opacity: 0.5 }}
-          >
-            <MenuItem>ヒットソングメドレーくじ</MenuItem>
-          </Link>
-          <Link
-            as={NextLink}
-            href={PAGE_INFO.JOHNNYS_ROULETTE.path}
-            _hover={{ textDecoration: "none", opacity: 0.5 }}
-          >
-            <MenuItem>ジャニーズルーレット</MenuItem>
-          </Link>
-        </MenuList>
-      </Menu>
-    </Flex>
+        <Link
+          as={NextLink}
+          href={PAGE_INFO.HOME.path}
+          _hover={{ textDecoration: "none", opacity: 0.5 }}
+        >
+          <Text fontWeight={800} fontSize="18px">
+            カラオケゲーム
+          </Text>
+        </Link>
+        <Menu>
+          <MenuButton
+            as={IconButton}
+            aria-label="Options"
+            icon={<GiHamburgerMenu />}
+            variant="outline"
+          />
+          <MenuList>
+            <Link
+              as={NextLink}
+              href={PAGE_INFO.COOPERATION.path}
+              _hover={{ textDecoration: "none", opacity: 0.5 }}
+            >
+              <MenuItem>協力ビンゴ</MenuItem>
+            </Link>
+            <Link
+              as={NextLink}
+              href={PAGE_INFO.MATCH.path}
+              _hover={{ textDecoration: "none", opacity: 0.5 }}
+            >
+              <MenuItem>対戦ビンゴ</MenuItem>
+            </Link>
+            <Link
+              as={NextLink}
+              href={PAGE_INFO.MEDLEY.path}
+              _hover={{ textDecoration: "none", opacity: 0.5 }}
+            >
+              <MenuItem>ヒットソングメドレーくじ</MenuItem>
+            </Link>
+            <Link
+              as={NextLink}
+              href={PAGE_INFO.JOHNNYS_ROULETTE.path}
+              _hover={{ textDecoration: "none", opacity: 0.5 }}
+            >
+              <MenuItem>ジャニーズルーレット</MenuItem>
+            </Link>
+          </MenuList>
+        </Menu>
+      </Flex>
+    </Box>
   );
 };
